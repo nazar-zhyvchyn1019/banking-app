6fd25d7f0733a4d3e6d5e7c4b9fa48930b0de9ae
Nazar Zhyvchyn <nazar.zhyvchyn1019@gmail.com>
2024-12-05 22:13:00 +0100

    feat: vercel analytics

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Fragment } from "react";
+import { Analytics } from "@vercel/analytics/react";
+
 import { UserContextProvider } from "@/contexts/user";
 
 export function ContextProvider({ children, ...props }) {
     return (
         <Fragment {...props}>
             <UserContextProvider>{children}</UserContextProvider>
+            <Analytics />
         </Fragment>
     );
 }
